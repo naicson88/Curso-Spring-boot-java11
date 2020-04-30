d7dafd801328ba452f1f70bdba2ee4e8bfca9568
naicson88 <naicson88@hotmail.com>
2020-04-30 17:44:52 -0300

    aula2

---
+++ src/main/java/com/example/curso/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "Mariaemail", "333-444", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
