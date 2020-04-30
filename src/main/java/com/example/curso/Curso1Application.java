b52cd2c8b6f6904aea87c8936b733c53bc51cbef
naicson88 <naicson88@hotmail.com>
2020-04-30 17:28:28 -0300

    Criado

---
+++ src/main/java/com/example/curso/Curso1Application.java
@@ -0,0 +1,13 @@
+package com.example.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curso1Application.class, args);
+	}
+
+}
